c7d6919aa2918be222cdbd34ec8a46ceed391ee6
bcasey266 <bcasey266@gmail.com>
2015-10-04 01:28:39 -0500

    GoDaddy Change and Site Master
    Configured the website to work with GoDaddy hosting
    Worked on the configuration of the master screen
    -master screen needs work including color palette, CSS work, and layout
    fixes

--- DeltaTime/App_Code/Startup.Auth.cs
+++ DeltaTime/App_Code/Startup.Auth.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Owin;
 
 namespace DeltaTime
 {
-    public partial class Startup {
-
+    public partial class Startup
+    {
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301883
         public void ConfigureAuth(IAppBuilder app)
         {
