379177652e2d1261759b8a5505ce5e0b8f55f191
bcasey266 <bcasey266@gmail.com>
2015-10-13 22:24:40 -0500

    Github Changes 2

---
+++ DeltatimeApp/DeltatimeApp/MainMenu.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
+namespace DeltatimeApp
+{
+    public partial class MainMenu : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                StatusText.Text = string.Format("Hello {0}!!", User.Identity.GetUserName());
+                LnkTimeSheet.Visible = true;
+                LnkViewReports.Visible = true;
+                LnkLogOff.Visible = true;
+            }
+            else
+            {
+                LnkTimeSheet.Visible = false;
+                LnkViewReports.Visible = false;
+                LnkLogOff.Visible = false;
+            }
+        }
+    }
+}
